34d171019025107bd4f3884f7b7715970453ef03
benn0r <admin@benn0r.ch>
2012-03-12 21:39:07 +0100

    staging test

---
+++ staging.php
@@ -0,0 +1,39 @@
+<?php
+
+function modified($content) {
+	$lines = explode("\n", $content);
+	$files = array();
+
+	foreach ($lines as $line) {
+		if (strstr($line, ' M ')) {
+			$files[] = trim(str_replace(' M ', '', $line));
+		}
+	}
+
+	return $files;
+}
+
+function untracked($content) {
+	$lines = explode("\n", $content);
+	$files = array();
+	
+	foreach ($lines as $line) {
+		if (strstr($line, '?? ')) {
+			$files[] = trim(str_replace('?? ', '', $line));
+		}
+	}
+	
+	return $files;
+}
+
+$content = shell_exec('"C:\\Program Files (x86)\\Git\\bin\\git" status --porcelain --untracked-files=all 2>&1');
+
+echo '<p><strong>Modified:</strong><br />';
+foreach (modified($content) as $file) {
+	echo $file . '<br />';
+}
+echo '</p><p><strong>Untracked:</strong><br />';
+foreach (untracked($content) as $file) {
+	echo $file . '<br />';
+}
+echo '</p>';
